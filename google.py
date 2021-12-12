8ca9d8db0fa279fe74a0e561c7070bc0d90c4806
Sayed-Aminul-au27 <89654789+Sayed-Aminul-au27@users.noreply.github.com>
2021-12-12 11:42:24 +0530

    Update google.py

--- google.py
+++ google.py
@@ -92,4 +92,341 @@ This apple is red and its flavor is sweet
 
 This apple is red and its flavor is sweet
 
-It's good practice to think about how your class might be used and to define a __str__ method when creating objects that you may want to print later.
+It's good practice to think about how your class might be used and
+to define a __str__ method when creating objects that you may want to print later.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Documenting with Docstrings
+
+The Python help function can be super helpful for easily pulling up documentation for classes and methods. 
+We can call the help function on one of our classes,
+which will return some basic info about the methods defined in our class:
+
+>>> class Apple:
+...     def __init__(self, color, flavor):
+...         self.color = color
+...         self.flavor = flavor
+...     def __str__(self):
+...         return "This apple is {} and its flavor is {}".format(self.color, self.flavor)
+... 
+>>> help(Apple)
+Help on class Apple in module __main__:
+
+class Apple(builtins.object)
+ |  Methods defined here:
+ |  
+ |  __init__(self, color, flavor)
+ |      Initialize self.  See help(type(self)) for accurate signature.
+ |  
+ |  __str__(self)
+ |      Return str(self).
+ |  
+ |  ----------------------------------------------------------------------
+ |  Data descriptors defined here:
+ |  
+ |  __dict__
+ |      dictionary for instance variables (if defined)
+ |  
+ |  __weakref__
+ |      list of weak references to the object (if defined)
+
+  
+  
+  
+  
+  
+  
+  
+  
+  We can add documentation to our own classes, methods, and functions using docstrings. 
+  A docstring is a short text explanation of what something does. You can add a docstring to a method, function, 
+  or class by first defining it, then adding a description inside triple quotes. Let's take the example of this function:
+  
+  >>> def to_seconds(hours, minutes, seconds):
+...     """Returns the amount of seconds in the given hours, minutes and seconds."""
+...     return hours*3600+minutes*60+seconds
+... 
+
+
+
+We have our function called to_seconds on the first line, followed by the docstring which is indented to the right 
+and wrapped in triple quotes. Last up is the function body. Now, when we call the help function on our to_seconds function,
+we get a handy description of what the function does:
+
+>>> help(to_seconds)
+Help on function to_seconds in module __main__:
+
+to_seconds(hours, minutes, seconds)
+    Returns the amount of seconds in the given hours, minutes and seconds.
+
+  
+  
+  
+  
+  Docstrings are super useful for documenting our custom classes, methods, and functions, 
+  but also when working with new libraries or functions. 
+  You'll be extremely grateful for docstrings when you have to work with code that someone else wrote!
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+Classes and Methods Cheat Sheet (Optional)
+
+
+
+Classes and Methods Cheat Sheet
+
+In the past few videos, we’ve seen how to define classes and methods in Python. Here, you’ll find a run-down of everything we’ve covered, so you can refer to it whenever you need a refresher.
+Defining classes and methods
+
+class ClassName:
+    def method_name(self, other_parameters):
+        body_of_method
+        
+        
+
+
+        
+        
+        
+Classes and Instances
+
+    Classes define the behavior of all instances of a specific class.
+
+    Each variable of a specific class is an instance or object.
+
+    Objects can have attributes, which store information about the object.
+
+    You can make objects do work by calling their methods.
+
+    The first parameter of the methods (self) represents the current instance.
+
+    Methods are just like functions, but they can only be used through a class.
+    
+    
+    
+    
+    
+    
+    
+Special methods
+
+    Special methods start and end with __.
+
+    Special methods have specific names, like __init__ for the constructor or __str__ for the conversion to string.
+    
+    
+    
+    
+    
+    
+    
+    
+Documenting classes, methods and functions
+
+    You can add documentation to classes, methods, and functions by using docstrings right after the definition. Like this:
+    
+    
+    
+class ClassName:
+    """Documentation for the class."""
+    def method_name(self, other_parameters):
+        """Documentation for the method."""
+        body_of_method
+        
+def function_name(parameters):
+    """Documentation for the function."""
+    body_of_function
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+Object Inheritance
+
+In object-oriented programming, the concept of inheritance allows you to build relationships between objects, 
+grouping together similar concepts and reducing code duplication. 
+Let's create a custom Fruit class with color and flavor attributes:
+
+
+>>> class Fruit:
+...     def __init__(self, color, flavor):
+...         self.color = color
+...         self.flavor = flavor
+...
+
+
+
+
+5
+
+We defined a Fruit class with a constructor for color and flavor attributes. Next, 
+we'll define an Apple class along with a new Grape class,
+both of which we want to inherit properties and behaviors from the Fruit class:
+  
+  
+  >>> class Apple(Fruit):
+...     pass
+... 
+>>> class Grape(Fruit):
+...     pass
+... 
+
+
+
+
+
+
+In Python, we use parentheses in the class declaration to have the class inherit from the Fruit class. 
+So in this example, we’re instructing our computer that both the Apple class and Grape class inherit from the Fruit class. 
+This means that they both have the same constructor method which sets the color and flavor attributes. 
+We can now create instances of our Apple and Grape classes:
+    
+    
+    
+    
+    >>> granny_smith = Apple("green", "tart")
+>>> carnelian = Grape("purple", "sweet")
+>>> print(granny_smith.flavor)
+tart
+>>> print(carnelian.color)
+purple
+
+
+
+
+
+
+
+
+Inheritance allows us to define attributes or methods that are shared by all types of fruit without 
+having to define them in each fruit class individually. 
+We can then also define specific attributes or methods that are only relevant for a specific type of fruit. 
+Let's look at another example, this time with animals:
+
+
+
+>>> class Animal:
+...     sound = ""
+...     def __init__(self, name):
+...         self.name = name
+...     def speak(self):
+...         print("{sound} I'm {name}! {sound}".format(
+...             name=self.name, sound=self.sound))
+... 
+>>> class Piglet(Animal):
+...     sound = "Oink!"
+... 
+>>> class Cow(Animal):
+...     sound = "Moooo"
+...
+
+
+
+
+
+
+
+
+We defined a parent class, Animal, with two animal types inheriting from that class: Piglet and Cow. 
+  The parent Animal class has an attribute to store the sound the animal makes, and the constructor class takes the name 
+  that will be assigned to the instance when it's created. There is also the speak method, 
+  which will print the name of the animal along with the sound it makes. We defined the Piglet and Cow classes, 
+  which inherit from the Animal class, and we set the sound attributes for each animal type. 
+  Now, we can create instances of our Piglet and Cow classes and have them speak:
+
+
+
+
+>>> hamlet = Piglet("Hamlet")
+>>> hamlet.speak()
+Oink! I'm Hamlet! Oink!
+... 
+>>> class Cow(Animal):
+...     sound = "Moooo"
+... 
+>>> milky = Cow("Milky White")
+>>> milky.speak()
+Moooo I'm Milky White! Moooo
+    
+  
+
+  
+ We create instances of both the Piglet and Cow class, and set the names for our instances. 
+  Then we call the speak method of each instance, which results in the formatted string being printed; 
+  it includes the sound the animal type makes, 
+  along with the instance name we assigned.
+    
+
+  
