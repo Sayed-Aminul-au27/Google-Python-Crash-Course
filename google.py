707d825bb6a52f66c5bb16bae616a33a6418ad7a
Sayed-Aminul-au27 <89654789+Sayed-Aminul-au27@users.noreply.github.com>
2021-12-12 12:21:54 +0530

    Update google.py

--- google.py
+++ google.py
@@ -644,3 +644,22 @@ This has the effect of adding 28 days to our original datetime object.
     
 
   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+Supplemental Reading for Code Reuse (Optional)
+
+
+Supplemental Reading for Code Reuse
+
+The official Python documentation lists all the modules included in the standard library. It even has a turtle in it... 
+
+Pypi(https://pypi.org) is the Python repository and index of an impressive number of modules developed by Python programmers around the world. 
